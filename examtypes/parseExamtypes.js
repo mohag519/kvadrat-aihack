60dbb992cc972de8c13283096f02a3cbc812606d
Mohammed Aghili <mohag519@gmail.com>
2023-06-02 12:16:34 +0200

    Trying hf + python etc.

--- examtypes/parseExamtypes.js
+++ examtypes/parseExamtypes.js
@@ -1,10 +1,8 @@
 import { OpenAI } from 'langchain/llms/openai'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { HuggingFaceInferenceEmbeddings } from 'langchain/embeddings/hf'
-import { HNSWLib } from 'langchain/vectorstores/hnswlib'
+import { FaissStore } from 'langchain/vectorstores/faiss'
 import { CSVLoader } from 'langchain/document_loaders/fs/csv'
 import { loadQAMapReduceChain } from 'langchain/chains'
-
+import { HuggingFaceInferenceEmbeddings } from 'langchain/embeddings/hf'
 import { ConversationalRetrievalQAChain } from 'langchain/chains'
 import { BufferMemory } from 'langchain/memory'
 
@@ -14,65 +12,31 @@ import url from 'url'
 import fs from 'fs'
 
 dotenv.config()
-const storeName = './examtypes-store'
 const openai = new OpenAI({ temperature: 0 })
 const memory = new BufferMemory({
   memoryKey: 'chat_history', // Must be set to "chat_history"
 })
-/* const embeddings = new OpenAIEmbeddings({ modelName: 'text-embedding-ada-002' }) */
-const embeddings = new HuggingFaceInferenceEmbeddings({
-  model: 'facebook/dino-vitb16',
-  maxRetries: 3,
-})
+
 const chain = loadQAMapReduceChain(openai, {
   returnIntermediateSteps: true,
   verbose: true,
 })
 
-const loadDocuments = async () => {
-  const loader = new CSVLoader('./anamneses.csv')
-  const docs = await loader.load()
-  return docs
-}
-
-const createStore = async () => {
-  var docs = await loadDocuments()
-  var store = await HNSWLib.fromDocuments(docs, embeddings)
-  console.log('Store created')
-  store.save(storeName)
-
-  return store
-}
-
 const loadStore = async () => {
-  console.log('Loading vector store')
-  return HNSWLib.load(storeName, embeddings).catch(() => {
-    console.log('Store did not exist. Creating store...')
-    return createStore()
+  const embeddings = new HuggingFaceInferenceEmbeddings({
+    model: 'diptanuc/all-mpnet-base-v2',
   })
+  return FaissStore.loadFromPython('../hfmodel/faiss_index', embeddings)
 }
 
+console.log('Loading vector store')
 const store = await loadStore()
-
-const memChain = ConversationalRetrievalQAChain.fromLLM(
-  openai,
-  store.asRetriever(8),
-  {
-    verbose: false,
-    returnSourceDocuments: false,
-    memory: memory,
-  }
-)
-
-await memChain.call({
-  question: `You are a bot that will receive descriptions of hospital patients and their symptoms.
-            Try to find types of examinations to conduct for the patients anamnes if possible. 
-            If you can't find a suiting exam type then say "I don't know"`,
-})
+console.log('Vector store loaded')
 
 const run = async (query) => {
-  /* const result = await chain.call({
-    input_documents: await store.similaritySearch(query),
+  const relevantDocs = await store.similaritySearch(query, 4)
+  const result = await chain.call({
+    input_documents: relevantDocs,
     question: `
       Answer as shortly as possible without omitting any Examination Type information 
       
@@ -80,19 +44,19 @@ const run = async (query) => {
       ${query}`,
   })
   console.log(result)
-  return result */
+  return result
 
-  return memChain.call({
+  /* return memChain.call({
     question: `${query}`,
-  })
+  }) */
 }
 
+console.log('Starting server')
 //Simplest node server
 const server = http
   .createServer(async (req, res) => {
-    res.statusCode = 200
-
     if (req.url === '/examtypes/index.html') {
+      res.statusCode = 200
       res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' })
       fs.createReadStream('index.html').pipe(res)
     } else if (req.url.startsWith('/examtypes/query?input=')) {
@@ -102,10 +66,13 @@ const server = http
       var query = url_parts.query
 
       var result = await run(query.input)
+      res.statusCode = 200
       res.end(result.text)
     } else {
       res.statusCode = 501
       res.end()
     }
   })
-  .listen(8123, '127.0.0.1', () => {})
+  .listen(8123, '127.0.0.1', () => {
+    console.log('Server started. Listening on "127.0.0.1:8123')
+  })
--- hfmodel/hfmodel.py
+++ hfmodel/hfmodel.py
@@ -1,6 +1,20 @@
-from sentence_transformers import SentenceTransformer
-sentences = ["This is an example sentence", "Each sentence is converted"]
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-embeddings = model.encode(sentences)
-print(embeddings)
+# sentences = ["This is an example sentence", "Each sentence is converted"]
+loader = CSVLoader(
+    file_path="../examtypes/anamneses.csv",
+    csv_args={
+        "delimiter": ",",
+        "quotechar": '"',
+        "fieldnames": ["Anamnes", "Examination Type"],
+    },
+    encoding="utf8",
+)
+data = loader.load()
+data.pop(0)
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.from_documents(data, embeddings)
+db.save_local("faiss_index")
